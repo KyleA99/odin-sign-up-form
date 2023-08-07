7f976d445d2d158b99ab44e4926925964d979042
Kyle Angelich <kyleangelich1@gmail.com>
2023-08-06 18:32:41 -0700

    Refactor password confirmation display

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ function passwordConfirmation() {
     const password = document.getElementById("password").value;
     const confirmedPassword = document.getElementById("confirmed-password").value;
    
-    if (password == "") {
-        alert("Error: The password field is Empty.");
+    if (password == "" || confirmedPassword == "") {
+        return ("Error: The password field is empty.");
     } else if (password == confirmedPassword) {
-        alert("Logged In");
+        return ("Your account has been successfully created. Please verify your email to confirm the account creation.");
     } else {
-        alert("Please make sure your passwords match.")
+        return ("Please make sure your passwords match.")
     }
+}
+
+function updatePasswordConfirmation() {
+    const passwordMatchDisplay = document.querySelector("#password-match-display");
+    const passwordMatchContent = document.createElement("div");
+    passwordMatchContent.classList.add("passwordMatchContent");
+    passwordMatchContent.textContent = passwordConfirmation();
+    passwordMatchDisplay.innerHTML = ''; // Clear previous content
+    passwordMatchDisplay.appendChild(passwordMatchContent);
 }
